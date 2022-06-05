db173dc569fd7a3a6b9a1965fb9f481c5f39a8ff
Muhammed Ülker <s80417@bht-berlin.de>
2022-06-06 00:31:38 +0200

    Updated test cases

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import unittest
 import common
+from selenium import webdriver
 
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
     def testUrlToSoup(self):
         self.assertTrue(common.urlToSoup("https://www.markt-pilot.de/en/home"))
         self.assertFalse(common.urlToSoup(""))
@@ -15,6 +20,19 @@ class Test(unittest.TestCase):
                      "combination": "", "url": ""}))
         self.assertFalse(common.saveProduct({"name": "", "price": "", "delivery": "", "needleSize": "",
                      "combination": ""}))
+
+    def testSearchWollplatz(self):
+        driver = self.driver
+        driver.get("https://www.wollplatz.de")
+        self.assertIn("Wollplatz", driver.title)
+
+    def testSearchDieZauberscheren(self):
+        driver = self.driver
+        driver.get("https://diezauberscheren.de")
+        self.assertIn("Zauberschere", driver.title)
+
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
