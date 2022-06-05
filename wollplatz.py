1fc5e2fdeabf8743883f4dce34f16d552c451a76
Muhammed Ülker <s80417@bht-berlin.de>
2022-06-06 00:18:59 +0200

    Removed unused modules

--- wollplatz.py
+++ wollplatz.py
@@ -1,10 +1,3 @@
-import urllib.parse
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 def getUrl(driver, product):
     driver.get(f"https://www.wollplatz.de/wolle?#sqr:(q[{product}])")
     driver.refresh()
